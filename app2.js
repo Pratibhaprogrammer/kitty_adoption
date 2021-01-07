9cfb7a26c29229c93e4bb165df15a0812c6c35d0
Jacob Holmer <yacub90@gmail.com>
2021-01-06 17:14:27 -0800

    index.html with added java script and css

---
+++ app2.js
@@ -0,0 +1,5 @@
+if (userName == 'Jacob'){
+    document.write('<h1>' + 'Welcome Jacob!' + '</h1>');
+} else if (userName != 'Jacob'){
+    document.write('<h3>' + 'Hello Gypsy' + '</h1>')
+}
---
+++ app3.js
@@ -0,0 +1,15 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'time space is broken';
+}
+
+document.write('<h2>' + greeting + '</h2>');
