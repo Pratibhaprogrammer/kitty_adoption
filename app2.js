9663cb27e565e285652d8d25582aaaa620786ff5
Jacob Holmer <yacub90@gmail.com>
2021-01-07 16:28:58 -0800

    added loop

--- app2.js
+++ app2.js
@@ -8,3 +8,11 @@ function checkName(userName){
 
 }
 checkName(userName);
+
+let catPictures = prompt('select a number');
+while (catPictures < 1 || catPictures > 10){
+    catPictures = prompt('please select a number between 1 and 10');
+}
+for(let i = 0; i < catPictures; i++){
+   document.write(' <img id="hero" src=https://static.toiimg.com/photo/msid-68523832/68523832.jpg>');
+}
